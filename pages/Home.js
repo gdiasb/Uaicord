a6446657367f45dee01221fb754407a04b45908b
gdiasb <gdias@peq.coppe.ufrj.br>
2022-01-26 18:15:50 -0300

    🎉 A new start!

---
+++ pages/Home.js
@@ -0,0 +1,144 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+
+export default function Home() {
+  const [username, setUsername] = React.useState("gdiasb");
+
+  return (
+    <>
+      <Box
+        styleSheet={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: appConfig.theme.colors.neutrals[300],
+          backgroundImage:
+            // "url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/blue-screen-of-death-in-windows-10.jpg)",
+            "url(https://images.unsplash.com/photo-1544456808-7530b2d112df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply",
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: {
+              xs: "column",
+              sm: "row",
+            },
+            width: "100%",
+            maxWidth: "700px",
+            borderRadius: "5px",
+            padding: "32px",
+            margin: "16px",
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+            backgroundColor: appConfig.theme.colors.neutrals[700],
+          }}
+        >
+          {/* Formulário */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: { xs: "100%", sm: "50%" },
+              textAlign: "center",
+              marginBottom: "32px",
+            }}
+          >
+            {/* <Title tag="h2">Bate-Papo Uai</Title> */}
+            <Text 
+              variant="heading1"
+              styleSheet={{
+                fontFamily: 'Jaman Edan',
+                color: appConfig.theme.colors.neutrals["050"]
+              }}>
+                Uai?!
+              </Text>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[300],
+              }}
+            >
+              {/* {appConfig.name} */}
+              Bora prosear um cadim?
+            </Text>
+
+            <TextField
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                },
+              }}
+              value={username}
+              onChange={({target}) => {
+                  setUsername(target.value)
+              }}
+            />
+            <Button
+              type="submit"
+              label="Entrar"
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "200px",
+              padding: "16px",
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: "1px solid",
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: "10px",
+              flex: 1,
+              minHeight: "240px",
+            }}
+          >
+            <Image
+              styleSheet={{
+                borderRadius: '50%',
+                marginBottom: '16px',
+              }}
+              src={(username.length > 2) ? `https://github.com/${username}.png` : `https://upload.wikimedia.org/wikipedia/commons/5/55/Question_Mark.svg`}
+            />
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: "3px 10px",
+                borderRadius: "1000px",
+              }}
+            >
+              {username}
+            </Text>
+          </Box>
+          {/* Photo Area */}
+        </Box>
+      </Box>
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import GlobalStyle from '../styles/GlobalStyle';
+import Home from './Home';
+
+export default function App(){
+return (
+    <>
+    <GlobalStyle />
+    <Home />
+    </>
+
+)
+}
