5cf6d3dfe187be11131c7a97b4497d8f1b2a0910
gdiasb <gdias@peq.coppe.ufrj.br>
2022-02-08 19:27:48 -0300

    ♻️ Move <Head> component to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import GlobalStyle from "../styles/GlobalStyle";
+import Head from "next/head";
 
 export default function Uaicord({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Uaicord Chat</title>
+        <meta property="og:title" content="Uaicord Chat" key="title" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import Home from "./home";
-import Head from "next/head";
 
 export default function App() {
   return (
     <>
-      <Head>
-        <title>Uaicord Chat</title>
-        <meta property="og:title" content="Uaicord Chat" key="title" />
-      </Head>
       <Home />
     </>
   );
