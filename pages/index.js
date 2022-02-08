292b5fd28ede8cb5d981838ca7e20605a97fb4fb
gdiasb <gdias@peq.coppe.ufrj.br>
2022-02-08 19:23:23 -0300

    ♿️ Add page title and meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Home from "./home";
+import Head from "next/head";
 
 export default function App() {
   return (
     <>
+      <Head>
+        <title>Uaicord Chat</title>
+        <meta property="og:title" content="Uaicord Chat" key="title" />
+      </Head>
       <Home />
     </>
   );
